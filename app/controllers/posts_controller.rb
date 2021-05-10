444c484f7e32f164a1307641044b98380ae9d2ea
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-05-10 19:25:52 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :show, :destroy]
   def create
     @post = Post.new(post_params)
     unless @post.save
@@ -6,7 +7,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+    @post.update(post_params)
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :content)
   end
